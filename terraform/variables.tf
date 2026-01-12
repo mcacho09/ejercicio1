3088b9826af37733514c0cdc9855a70a9add9901
mario cacho <mario.cacho09@outlook.com>
2026-01-12 13:04:08 -0600

    v2

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -2,13 +2,13 @@
 variable "bucket_name" {
   description = "The name of the S3 bucket"
   type        = string
-
+  default     = "mario-secure-bucket"
 }
 
 variable "region" {
   description = "AWS region to deploy resources"
   type        = string
-
+  default     = "us-east-1"
 }
 
 
